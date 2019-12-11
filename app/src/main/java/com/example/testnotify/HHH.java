151a66adc24540848e7b94b7d0da77de3a4deb79
namhoangle <happybear02042000@gmail.com>
2019-12-11 16:32:53 +0700

    first commit

---
+++ app/src/main/java/com/example/testnotify/HHH.java
@@ -0,0 +1,17 @@
+package com.example.testnotify;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static android.content.ContentValues.TAG;
+
+public class HHH extends FirebaseMessagingService {
+
+}
